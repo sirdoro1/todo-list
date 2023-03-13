9d35a5347a19caf2f52937d93edb43f3e81a0fd4
Dave Doro <sirdoro1@gmail.com>
2023-03-13 12:29:19 +0100

    update on Symfony app

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,17 +3,27 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     #[Route('/main', name: 'app_main')]
-    public function index(): Response
+    public function index(): JsonResponse
     {
-        return $this->([
+        return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/MainController.php',
+        ]);
+    }
+
+    #[Route('/number', name:'app_number')]
+    public function number():Response
+    {
+        $number = random_int(10000,999999);
+        return $this->render('/number.html.twig',[
+            'number' => $number
         ]);
     }
 }
